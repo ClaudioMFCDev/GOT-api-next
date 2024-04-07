48cd260d0127abd48db2c363d9f8e0abdd076329
ClaudioMFCDev <97706777+ClaudioMFCDev@users.noreply.github.com>
2024-04-07 20:59:30 -0300

    finished project

--- src/app/dashCard/[id]/page.tsx
+++ src/app/dashCard/[id]/page.tsx
@@ -1,21 +1,40 @@
-
+import Image from "next/image";
 
 const getData = async (id: any) => {
-        const response = await fetch(`https://thronesapi.com/api/v2/Characters/${id}`);
-        const data = await response.json();
-        // console.log(data)
-        return data || {};
-    }
-export default async function DashCard({params}: any) {
+  const response = await fetch(`https://thronesapi.com/api/v2/Characters/${id}`);
+  const data = await response.json();
+  // console.log(data)
+  return data || {};
+}
+export default async function DashCard({ params }: any) {
 
 
-    const data = await getData(params.id);
-    console.log(data)
+  const character = await getData(params.id);
 
   return (
     <main>
-        <div>{data.firstName}</div>
-        <div>Dash por id</div>
+      <div key={character.id} >
+        <div className=" group h- w- my-4 rounded-xl ring-gray-900/5 sm:mx-auto sm:max-w-lg">
+          <div className=" rounded-xl border border-gray-200">
+            <Image
+              // style={{ objectFit: 'cover', objectPosition: 'top' }}
+              className="h-full w-full scale-100 object-center opacity-100"
+              src={character.imageUrl}
+              alt="Character-image"
+              width={200}
+              height={200}
+            />
+            <div style={{ backgroundColor: 'white', width: '70%'}} className="w-100 p-3 rounded-xl bottom-0 z-20 m-0 pb-4 ps-4 transition duration-300 ease-in-out group-hover:-translate-y-1 group-hover:translate-x-3 group-hover:scale-110 group-hover:opacity-100">
+              <h1 className="text-lg font-bold text-white text-red-500">{character.fullName}</h1>
+              <h2 className="text-m font-light text-gray-200 text-red-500">Family: {character.family}</h2>
+              <h2 className="text-m font-light text-gray-200 text-red-500">Title: {character.title}</h2>
+              
+            </div>
+
+          </div>
+        </div>
+      </div>
+
     </main>
   )
 }
--- src/components/DashboardForm.tsx
+++ src/components/DashboardForm.tsx
@@ -16,10 +16,10 @@ export default async function DashboardPage() {
         <main>
             <nav className="flex justify-between m-6 items-center flex-wrap my-8">
                 <h1 className="text-5xl">Game of Thones' Characters</h1>
-                <div className="flex flex-wrap">
+                {/* <div className="flex flex-wrap">
                     <input type="text" name="" id="" className="rounded-sm my-2" />
                     <button type="button" className="my-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm mx-2 px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" >Serch</button>
-                </div>
+                </div> */}
             </nav>
 
             <div className="flex place-content-around items-center gap-y-8 gap-x-2 flex-wrap">
