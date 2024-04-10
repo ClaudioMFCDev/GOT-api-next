e1ea4e8ad8448394152ff3caafc344fd8fc97866
ClaudioMFCDev <97706777+ClaudioMFCDev@users.noreply.github.com>
2024-04-10 17:09:57 -0300

    data empty supabase

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -1,15 +1,20 @@
 import { NextRequest, NextResponse } from "next/server";
-import users from "./users.json"
+import { createServerClient } from "@/utils/supabase/server";
 
 export const POST = async (req: NextRequest, res: NextResponse) => {
     const body = await req.json();
+    const supabase = createServerClient();
     const {user, password} = body;
 
-    const login = users.usuarios.find(element =>  element.user === user && element.password === password)
+    const users = await supabase.from('users').select('*');
+    console.log("usuario de supabase", users);
+
+    const login = users.data?.find((element) =>  element.username === user 
+            && element.password === password)
     
-    if(login){
+    if( login !== undefined){
         return Response.json({message: "Bienvenido"})
     }else{
         return Response.json({message: "User o pass incorrecto"})
     }
-} 
+}
