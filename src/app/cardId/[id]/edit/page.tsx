eab8a3047822b3e8589e243968db8cc9aa81a60f
ClaudioMFCDev <97706777+ClaudioMFCDev@users.noreply.github.com>
2024-05-02 12:04:12 -0300

    starting  editCard

---
+++ src/app/cardId/[id]/edit/page.tsx
@@ -0,0 +1,17 @@
+import EditCard from "@/components/EditChar";
+import { createServerClient } from "@/utils/supabase/server";
+
+export default async function Edit({params}: any) {
+    const supabase = createServerClient();
+    const { data } = await supabase
+        .from('character')
+        .select('*')
+        .eq('id', params.id)
+        .single();
+    
+    // enviamos objeto 'data' por parametro
+
+    return (
+        <EditCard character={data}/>
+    )
+}
